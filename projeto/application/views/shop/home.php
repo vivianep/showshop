eec2125cf07ec7e56ac34ddc33275a8c89c73f32
joelrlneto <joelrlneto@gmail.com>
2015-05-14 10:28:14 -0300

    Views da home e loja.

---
+++ projeto/application/views/shop/home.php
@@ -0,0 +1,28 @@
+<div class="row">
+	<div class="col-md-9" id="div-banner"></div>
+	<div class="col-md-3">
+		<div class="row">
+			<div class="col-md-12 item-banner" style="background-color:red"></div>
+			<div class="col-md-12 item-banner" style="background-color:yellow"></div>
+			<div class="col-md-12 item-banner" style="background-color:blue"></div>
+			<div class="col-md-12 item-banner" style="background-color:gray"></div>
+			<div class="col-md-12 item-banner" style="background-color:green"></div>
+		</div>
+	</div>
+</div>
+<div class="row" class="div-lojas">
+	<div class="col-md-2 item-loja"></div>
+	<div class="col-md-2 item-loja"></div>
+	<div class="col-md-2 item-loja"></div>
+	<div class="col-md-2 item-loja"></div>
+	<div class="col-md-2 item-loja"></div>
+	<div class="col-md-2 item-loja"></div>
+</div>
+<div class="row" class="div-lojas">
+	<div class="col-md-2 item-loja"></div>
+	<div class="col-md-2 item-loja"></div>
+	<div class="col-md-2 item-loja"></div>
+	<div class="col-md-2 item-loja"></div>
+	<div class="col-md-2 item-loja"></div>
+	<div class="col-md-2 item-loja"></div>
+</div>
---
+++ projeto/application/views/shop/loja.php
@@ -0,0 +1,28 @@
+<div class="row">
+	<div class="col-md-3">
+		<div class="row">
+			<div class="col-md-12" id="menu-loja">
+				<ul class="nav nav-pils nav-stack">
+					<li><a href="#">Calçados</a></li>
+					<li><a href="#">Roupas</a></li>
+					<li><a href="#">Eletrônicos</a></li>
+					<li><a href="#">Cama e mesa</a></li>
+					<li><a href="#">Utilidades</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-9" id="div-banner"></div>
+</div>
+<div class="row" class="div-produtos">
+	<div class="col-md-3 item-produto"></div>
+	<div class="col-md-3 item-produto"></div>
+	<div class="col-md-3 item-produto"></div>
+	<div class="col-md-3 item-produto"></div>
+</div>
+<div class="row" class="div-produtos">
+	<div class="col-md-3 item-produto"></div>
+	<div class="col-md-3 item-produto"></div>
+	<div class="col-md-3 item-produto"></div>
+	<div class="col-md-3 item-produto"></div>
+</div>
